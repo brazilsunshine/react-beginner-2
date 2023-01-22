0870ddae7ef17bcd87c8d1a4dfca42809747ad12
Sthefane <sthefanesoliveiraa@outlook.com>
2023-01-22 16:24:59 +0000

    Fetching Data with React Query

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import useFetch from "./useFetch";
+import {useQuery} from "react-query";
 
 export default function Joke () {
-    const { data: joke, isLoading, errorMessage } = useFetch('https://official-joke-api.appspot.com/jokes/random');
+    // const { data: joke, isLoading, errorMessage } = useFetch('https://official-joke-api.appspot.com/jokes/random');
+
+    const {
+        data: joke,
+        isLoading,
+        isError,
+        error,
+        isSuccess,
+    } = useQuery('joke', fetchJoke, { // third parameter is optional
+        staleTime: 6000,
+        refetchOnWindowFocus: false,
+        // by default refetchOnWindowFocus tells React to automatically request fresh data for you
+        // in the background when the user leaves your application and returns to stale data, so
+        // we need to set it to false to prevent this automatically request from happening
+    });
+
+    function fetchJoke ()
+    {
+        return fetch('https://official-joke-api.appspot.com/jokes/random')
+            .then(response => response.json());
+    }
+
 
     return (
         <div>
@@ -10,13 +31,13 @@ export default function Joke () {
 
             { isLoading && <div>Loading...</div> }
 
-            { joke &&
+            { isSuccess &&
                 <div>
                     { joke.setup + '' + joke.punchline }
                 </div>
             }
 
-            {errorMessage && <div>{ errorMessage }</div>}
+            {isError && <div>{ error.message }</div>}
 
         </div>
     )
--- src/Reddit.jsx
+++ src/Reddit.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import useFetch from "./useFetch";
+import {useQuery} from "react-query";
 
 export default function Reddit () {
-    const { data: posts, isLoading, errorMessage } = useFetch('http://www.reddit.com/r/aww.json');
+    // const { data: posts, isLoading, errorMessage } = useFetch('http://www.reddit.com/r/aww.json');
+
+     const {
+         data: posts,
+         isLoading,
+         isError,
+         error,
+         isSuccess,
+
+     } = useQuery('posts', fetchPosts, { // third parameter is optional
+         retry: false // will prevent React from trying to fetch the data multiple times
+     });
+
+     function fetchPosts ()
+     {
+         return  fetch('https://www.reddit.com/r/aww.json')
+             .then(response => response.json())
+     }
+
 
     return (
         <div>
@@ -10,7 +28,7 @@ export default function Reddit () {
 
             {isLoading && <div>Loading...</div>}
 
-            {posts && (
+            {isSuccess &&
                 <ul>
                     {posts.data.children.map(post => (
                         <li key={post.data.id}>
@@ -20,9 +38,9 @@ export default function Reddit () {
                         </li>
                     ))}
                 </ul>
-            )}
+            }
 
-            {errorMessage && <div>{errorMessage}</div>}
+            {isError && <div>{error.message}</div>}
 
         </div>
     )
